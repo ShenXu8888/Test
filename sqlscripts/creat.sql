8cc68d233f1bb105315c7fe0bc23c753bcd616fb
ShenXu8888 <90271342+ShenXu8888@users.noreply.github.com>
2021-09-20 15:30:41 +0100

    Update creat.sql

--- sqlscripts/creat.sql
+++ sqlscripts/creat.sql
@@ -1,8 +1,7 @@
 USE [CaboodlePOC_Stage]
 
-create table Custom.SXu_ErsatzEmpolyeeRole (
-	StageKey				int IDENTITY(1,1)	--Technically optional	
-	,eMPLOYEE_ROLE_ID					nvarchar(10)
+create table Custom.SXu_ErsatzEmpolyeeRole (	StageKey				int IDENTITY(1,1)	--Technically optional	
+					    ,eMPLOYEE_ROLE_ID					nvarchar(10)
 	,eMPLOYEE_ROLE_NAME					nvarchar(50)
 	,EMPLOYEE_ROLE_DESCRIPTION				nvarchar(300)
 	,EMPLOYEE_CLASS				nvarchar(100)
