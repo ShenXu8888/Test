ce36e6b1940923978ea278505ebfc29102c61960
ShenXu8888 <90271342+ShenXu8888@users.noreply.github.com>
2021-09-20 15:28:20 +0100

    Update SqlScript.sql

--- SqlScript.sql
+++ SqlScript.sql
@@ -1,4 +1,10 @@
-SELECT
-  *
-FROM
-  test;
+USE [CaboodlePOC_Stage]
+
+CREATE TABLE Custom.SXu_ErsatzEmpolyeeRole (
+	StageKey				int IDENTITY(1,1)	--Technically optional	
+	,eMPLOYEE_ROLE_ID					nvarchar(10)
+	,EMPLOYEE_ROLE_NAME					nvarchar(50)
+	,EMPLOYEE_ROLE_DESCRIPTION				nvarchar(300)
+	,EMPLOYEE_CLASS				nvarchar(100)
+	,LoadDate					date
+	)
