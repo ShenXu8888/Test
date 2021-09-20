643686e79b785bd96530299b823d2568a100ecb6
ShenXu8888 <90271342+ShenXu8888@users.noreply.github.com>
2021-09-20 15:28:58 +0100

    Create creat.sql

---
+++ sqlscripts/creat.sql
@@ -0,0 +1,10 @@
+USE [CaboodlePOC_Stage]
+
+CREATE TABLE Custom.SXu_ErsatzEmpolyeeRole (
+	StageKey				int IDENTITY(1,1)	--Technically optional	
+	,eMPLOYEE_ROLE_ID					nvarchar(10)
+	,eMPLOYEE_ROLE_NAME					nvarchar(50)
+	,EMPLOYEE_ROLE_DESCRIPTION				nvarchar(300)
+	,EMPLOYEE_CLASS				nvarchar(100)
+	,LoadDate					date
+	)
